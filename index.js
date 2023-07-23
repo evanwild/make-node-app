fbeefce4c49ab6f296ad3f5d4b4978b9a3b3cf11
Evan Wild <evan@evanwild.com>
2023-07-23 12:01:28 -0400

    refactor

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 #!/usr/bin/env node
 
-import './makeNodeApp.js';
+import { input } from '@inquirer/prompts';
+import { dirname, resolve } from 'node:path';
+import { cp, readFile, writeFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+
+const projectDir = await input({
+  message: 'Project name:',
+  default: 'node-project',
+});
+
+const absolutePath = resolve(projectDir);
+console.log(`\nScaffolding project in ${absolutePath}...\n`);
+
+const moduleDir = fileURLToPath(dirname(import.meta.url));
+await cp(`${moduleDir}/templates/vanilla-ts/`, projectDir, { recursive: true });
+
+// From https://github.com/vitejs/vite/tree/main/packages/create-vite
+const packageName = projectDir
+  .trim()
+  .toLowerCase()
+  .replace(/\s+/g, '-')
+  .replace(/^[._]/, '')
+  .replace(/[^a-z\d\-~]+/g, '-');
+
+const packageContents = await readFile(`${projectDir}/package.json`, {
+  encoding: 'utf8',
+});
+
+// Replace the temp "name" field in package.json
+const newContents = packageContents.replace(
+  /  "name": "(.*)",\n/,
+  `  "name": "${packageName}",\n`
+);
+
+await writeFile(`${projectDir}/package.json`, newContents);
+
+console.log('Done. Now run:\n');
+console.log(`  cd ${projectDir}`);
+console.log(`  npm install`);
+console.log(`  npm run start\n`);
